192e70710b562feaffa6c55398b5af2ea2c96ea8
Dmitriy G <llogginn@gmail.com>
2024-05-08 00:02:05 +0500

    acsiinema step3

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 [![Github Actions Status](https://github.com/braimm/python-project-50/workflows/Python%20CI/badge.svg)](https://github.com/braimm/python-project-50/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/9aadb67621f709d2eac9/maintainability)](https://codeclimate.com/github/braimm/python-project-50/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/9aadb67621f709d2eac9/test_coverage)](https://codeclimate.com/github/braimm/python-project-50/test_coverage)
+
+
+
+
+1. **Step 3**. Generate diff STYLISH-format for plain JSON-files:
+https://asciinema.org/a/wNkLM7Nf7ipJ2bWwPVRGaY2Pu
+[![asciicast](https://asciinema.org/a/wNkLM7Nf7ipJ2bWwPVRGaY2Pu.svg)](https://asciinema.org/a/wNkLM7Nf7ipJ2bWwPVRGaY2Pu)
+
+2. **Step 5**. Generate diff STYLISH-format for plain YAML-files:
+https://asciinema.org/a/DB5PK2dLAdxfnKfwO3nMlUOJ7
+[![asciicast](https://asciinema.org/a/DB5PK2dLAdxfnKfwO3nMlUOJ7.svg)](https://asciinema.org/a/DB5PK2dLAdxfnKfwO3nMlUOJ7)
+
+3. **Step 6**. Generate diff in STYLISH-format for nested YAML/JSON-files:
+https://asciinema.org/a/nT4INAMhmMTnJcB8esbmvFeYC
+[![asciicast](https://asciinema.org/a/nT4INAMhmMTnJcB8esbmvFeYC.svg)](https://asciinema.org/a/nT4INAMhmMTnJcB8esbmvFeYC)
+
+4. **Step 7**. Generate diff in PLAIN-format for nested YAML/JSON-files:
+https://asciinema.org/a/fmPCr6Ao4U1szMdz1ojBqfMBY
+[![asciicast](https://asciinema.org/a/fmPCr6Ao4U1szMdz1ojBqfMBY.svg)](https://asciinema.org/a/fmPCr6Ao4U1szMdz1ojBqfMBY)
+
+5. **Step 8**. Generate diff in JSON-format for nested YAML/JSON-files:
+https://asciinema.org/a/KL26zMR27IzuQfXDspIRbAlyS
+[![asciicast](https://asciinema.org/a/KL26zMR27IzuQfXDspIRbAlyS.svg)](https://asciinema.org/a/KL26zMR27IzuQfXDspIRbAlyS)
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,28 +1,37 @@
-# import pytest
-# import os
+import pytest
 from gendiff.generate_diff import generate_diff
 
 
-def test_generate_diff():
-    diff_file = open('tests/fixtures/diff.txt')
-    reference = diff_file.read()
-    diff_file.close()
+@pytest.fixture
+def reference_stylish():
+    fixture_file = open('tests/fixtures/result_stylish.txt')
+    fixture = fixture_file.read()
+    fixture_file.close()
+    return fixture
+
+
+
+def test_generate_diff_json(reference_stylish):
     path_file_1 = 'tests/fixtures/file1.json'
     path_file_2 = 'tests/fixtures/file2.json'
     result = generate_diff(path_file_1, path_file_2)
-    assert result == reference
+    assert result == reference_stylish
 
+
+def test_generate_diff_yaml(reference_stylish):
     path_file_1 = 'tests/fixtures/file1.yml'
     path_file_2 = 'tests/fixtures/file2.yml'
     result = generate_diff(path_file_1, path_file_2)
-    assert result == reference
+    assert result == reference_stylish
 
     path_file_1 = 'tests/fixtures/file1.yaml'
     path_file_2 = 'tests/fixtures/file2.yaml'
     result = generate_diff(path_file_1, path_file_2)
-    assert result == reference
+    assert result == reference_stylish
 
-    diff_nested_file = open('tests/fixtures/diff_nested.txt')
+
+def test_generate_diff_nested_to_stylish():
+    diff_nested_file = open('tests/fixtures/result_nested_stylish.txt')
     reference = diff_nested_file.read()
     diff_nested_file.close()
     path_file_1 = 'tests/fixtures/file_nested_1.json'
@@ -30,7 +39,9 @@ def test_generate_diff():
     result = generate_diff(path_file_1, path_file_2)
     assert result == reference
 
-    diff_nested_to_plain_file = open('tests/fixtures/diff_nested_to_plain.txt')
+
+def test_generate_diff_nested_to_plain():
+    diff_nested_to_plain_file = open('tests/fixtures/result_plain.txt')
     reference = diff_nested_to_plain_file.read()
     diff_nested_to_plain_file.close()
     path_file_1 = 'tests/fixtures/file_nested_1.json'
