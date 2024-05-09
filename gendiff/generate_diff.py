64ac27854b70c70cec9d1bb2d4ff95cb8310151d
Dmitriy G <llogginn@gmail.com>
2024-05-09 19:13:56 +0500

    fix codeclimate's notice

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,24 +3,13 @@ from gendiff.formatters.generator_output import generate_output
 
 
 def get_sets_keys(data_1, data_2):
-    keys_only_data_1 = set()
-    keys_only_data_2 = set()
-    viewed_keys = set()
-    for key_data_1 in data_1.keys():
-        if key_data_1 not in data_2.keys():
-            keys_only_data_1.add(key_data_1)
-            viewed_keys.add(key_data_1)
-        else:
-            viewed_keys.add(key_data_1)
+    keys_data_1 = set(data_1.keys())
+    keys_data_2 = set(data_2.keys())
+    all_keys = keys_data_1 | keys_data_2
+    keys_only_data_1 = keys_data_1 - keys_data_2
+    keys_only_data_2 = keys_data_2 - keys_data_1
 
-    for key_data_2 in data_2.keys():
-        if key_data_2 not in data_1.keys():
-            keys_only_data_2.add(key_data_2)
-            viewed_keys.add(key_data_2)
-        else:
-            viewed_keys.add(key_data_2)
-
-    return viewed_keys, keys_only_data_1, keys_only_data_2
+    return all_keys, keys_only_data_1, keys_only_data_2
 
 
 def get_diff(data_1, data_2):
